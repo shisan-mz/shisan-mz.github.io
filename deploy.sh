08149ea7301ca29e916bafc2b41701024f4b6b94
liwei <2074605122@qq.com>
2024-06-12 17:05:58 +0800

    feat code

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:Libaizhu/Libaizhu.github.io.git master  
+# git push -f git@github.com:Libaizhu/Libaizhu.github.io.git master  
+git push -f git@github.com:shisan-mz/shisan-mz.github.io.git master  
 
 cd -
 
